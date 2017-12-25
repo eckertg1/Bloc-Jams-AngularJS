4359f9378fafbcfc13d532e00347ea1c055dfc48
Geoff <eckertg1@yahoo.com>
2017-12-25 00:24:37 -0500

    Checkipoint 3 routing and states complete

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,29 @@
-angular.module('blocJams', []);
+(function(){
+  function config($stateProvider, $locationProvider){
+    $locationProvider
+      .html5Mode({
+        enabled: true,
+        requiredBase: false
+      });
+
+    $stateProvider
+      .state('landing', {
+        url: '/',
+        templateUrl: '/templates/landing.html'
+      })
+      .state('album', {
+        url: '/',
+        templateUrl: '/templates/album.html'
+      });
+
+    $stateProvider
+      .state('collection', {
+        url: '/',
+        templateUrl: '/templates/collection.html'
+      });
+  }
+
+  angular
+      .module('blocJams', ['ui.router'])
+      .config(config);
+})();
