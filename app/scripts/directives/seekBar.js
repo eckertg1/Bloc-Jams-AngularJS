1875ec7c16e51c10bda7de3fa2adbe74f4eb9a7b
Geoff <eckertg1@yahoo.com>
2018-01-24 14:31:25 -0500

    assignment-9-directives-part-1 complete

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -48,6 +48,10 @@
             $document.unbind('mouseup.thumb');
           });
         };
+
+        scope.thumbStyle = function(){
+          return{left: percentString()};
+        };
       }
     };
   }
